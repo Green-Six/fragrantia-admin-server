e57aaa8d3581a0fe363ebd9d49ad8590780cd51f
정익 <dlwjddlr97@gmail.com>
2023-07-21 12:33:15 +0900

    IK-17 footer 추가, test 코드 삭제

---
+++ src/main/resources/static/js/footer.js
@@ -0,0 +1,5 @@
+const bizno = document.getElementById('publisherNo')
+
+bizno.addEventListener('click',function(){
+    window.open('http://www.ftc.go.kr/bizCommPop.do?wrkr_no=3iuo434o32324', '_blank', 'width=750, height=950;');return false;
+})
